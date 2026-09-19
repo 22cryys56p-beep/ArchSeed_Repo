fe07e700f7d0cfe09edd49557acb5d48d749e1f3
22cryys56p-beep <22cryys56p@privaterelay.appleid.com>
2026-09-20 00:37:19 +0900

    Create operation.ts

---
+++ src/core/operation.ts
@@ -0,0 +1,19 @@
+import type { Outcome } from "./outcome";
+
+export type StateAuthority<State> = {
+  get(): State;
+  mutate(mutator: (state: State) => void): void;
+};
+
+export type Operation<
+  Request,
+  State,
+  Dependencies,
+  Details = unknown
+> = (
+  request: Request,
+  context: {
+    state: StateAuthority<State>;
+    dependencies: Dependencies;
+  }
+) => Outcome<Details>;
